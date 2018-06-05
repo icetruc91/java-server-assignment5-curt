5dddc9ee7520217d308c921b9629c37285229510
lockhartc <lockhart.c@husky.neu.edu>
2018-06-04 21:44:29 -0400

    final product

--- src/main/java/com/example/myapp/models/User.java
+++ src/main/java/com/example/myapp/models/User.java
@@ -15,9 +15,30 @@ public class User {
 	private String firstName;
 	private String lastName;
 	private String role;
+	private String date;
+	private String phone;
+	private String email; 
 
 	
 	
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	public int getId() {
 		return id;
 	}
