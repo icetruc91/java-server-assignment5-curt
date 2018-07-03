9465dca0068a617b09e1d8dc0192b7f83e00fa7d
lockhartc <lockhart.c@husky.neu.edu>
2018-07-03 15:49:40 -0400

    created user admin repository

--- src/main/java/webdev/repositories/UserRepository.java
+++ src/main/java/webdev/repositories/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.query.Param;
 import webdev.models.User;
 
 
-
+// User Repository.
 public interface UserRepository extends CrudRepository<User, Integer>{
 	@Query("SELECT u FROM User u WHERE u.username=:username AND u.password=:password")
 	Iterable<User> findUserByCredentials(
