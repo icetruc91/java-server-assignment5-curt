edc92a5167986ac9eacc29d9ce90da9cd9f9f644
lockhartc <lockhart.c@husky.neu.edu>
2018-07-03 16:26:04 -0400

    Register posts to a register web service

--- src/main/webapp/jquery/services/user.service.client.js
+++ src/main/webapp/jquery/services/user.service.client.js
@@ -89,8 +89,7 @@ function UserServiceClient() {
         });
     }
 
-
-
+    // Register used to post to the server when a new patron registers.
     function register(user) {
         return fetch(self.registerUrl, {
             method: 'post',
