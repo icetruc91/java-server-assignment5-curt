6cee7d947e4e0f756b389565a41aa7d510769abf
lockhartc <lockhart.c@husky.neu.edu>
2018-07-03 15:55:04 -0400

    created user admin web service

--- src/main/java/webdev/services/UserService.java
+++ src/main/java/webdev/services/UserService.java
@@ -25,6 +25,51 @@ public class UserService {
 	@Autowired
 	UserRepository repository;
 	
+	// Creates a new user.
+	@PostMapping("/api/user")
+	public User createUser(@RequestBody User user) {
+		return repository.save(user);
+	}
+	
+	// Finds and returns a list of all users.
+	@GetMapping("/api/user")
+	public List<User> findAllUsers() {
+		return (List<User>) repository.findAll();
+	}
+	
+	// Finds user by id. 
+	@GetMapping("/api/user/id/{userId}")
+	public User findUserById(@PathVariable("userId") int userId) {
+		Optional<User> data = repository.findById(userId);
+		if(data.isPresent()) {
+			return data.get();
+		}
+		return null;
+	}
+	
+	// Updates an existing user. 
+	@PutMapping("/api/user/{userId}")
+	public User updateUser(@PathVariable("userId") int userId, @RequestBody User newUser) {
+		Optional<User> data = repository.findById(userId);
+		if(data.isPresent()) {
+			User user = data.get();
+			user.setFirstName(newUser.getFirstName());
+			user.setLastName(newUser.getLastName());
+			user.setPassword(newUser.getPassword());
+			user.setUsername(newUser.getUsername());
+			user.setRole(newUser.getRole());
+			repository.save(user);
+			return user;
+		}
+		return null;
+	}
+	
+	// Deletes a user. 
+	@DeleteMapping("/api/user/{userId}")
+	public void deleteUser(@PathVariable("userId") int id) {
+		repository.deleteById(id);
+	}
+	
 	@PostMapping("/api/login")
 	public User login(	@RequestBody User credentials,
 	HttpSession session) {
@@ -58,45 +103,15 @@ public class UserService {
 
 	
 	
-	@DeleteMapping("/api/user/{userId}")
-	public void deleteUser(@PathVariable("userId") int id) {
-		repository.deleteById(id);
-	}
 	
-	@PostMapping("/api/user")
-	public User createUser(@RequestBody User user) {
-		return repository.save(user);
-	}
 	
-	@GetMapping("/api/user")
-	public List<User> findAllUsers() {
-		return (List<User>) repository.findAll();
-	}
 	
-	@PutMapping("/api/user/{userId}")
-	public User updateUser(@PathVariable("userId") int userId, @RequestBody User newUser) {
-		Optional<User> data = repository.findById(userId);
-		if(data.isPresent()) {
-			User user = data.get();
-			user.setFirstName(newUser.getFirstName());
-			user.setLastName(newUser.getLastName());
-			user.setPassword(newUser.getPassword());
-			user.setUsername(newUser.getUsername());
-			user.setRole(newUser.getRole());
-			repository.save(user);
-			return user;
-		}
-		return null;
-	}
 	
-	@GetMapping("/api/user/id/{userId}")
-	public User findUserById(@PathVariable("userId") int userId) {
-		Optional<User> data = repository.findById(userId);
-		if(data.isPresent()) {
-			return data.get();
-		}
-		return null;
-	}
+	
+	
+	
+	
+	
 	
 	
 	@GetMapping("/api/user/username/{username}")
